7957b167c37b7b8c241490f24e7b044fe7943637
Krishna <krishnadk8203@gmail.com>
2023-01-21 14:18:16 +0530

    made api endpoints

---
+++ backend/controllers/Competition_controllers.js
@@ -0,0 +1,42 @@
+const Competetions = require("../models/competetions");
+
+const getCompetetions = async (req, res) =>{
+    try{
+        const data= await Competetions.find();
+        res.status(200).json(data);
+    }catch(err){
+        throw err;
+    }
+}
+
+const createCompetetion = async (req, res)=>{
+    try{
+        const data =  await Competetions.create(req.body);
+        res.status(200).json(data);
+    }catch(err){
+        throw err;
+    }
+    
+}
+
+const deleteCompetetion = async (req, res) =>{
+    const { id }= req.body;
+    try{
+        const data = await Competetions.findOneAndDelete({_id: id});
+        res.status(200).json(data);
+    }catch(err){
+        throw err;
+    }
+}
+
+const updateCompetetions = async (req, res) =>{
+    const { id }= req.body;
+    try{
+        const data = await Competetions.findOneAndUpdate({_id: id}, {...req.body});
+        res.status(200).json(data);
+    }catch(err){
+        throw err;
+    }
+}
+
+module.exports = {getCompetetions ,createCompetetion, deleteCompetetion, updateCompetetions};
